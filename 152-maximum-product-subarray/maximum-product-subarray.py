9e42b7d92a12359d014393b9fdf8c1e63f11cf2a
Harish Bandla <harishbandla409@gmail.com>
2026-08-10 21:37:27 +0530

    Time: 3 ms (90.62%) | Memory: 19.8 MB (78.32%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        minending = nums[0]
+        maxending =  nums[0]
+        res = nums[0]
+
+        for i in range(1,len(nums)):
+            v1 = nums[i] 
+            v2 = minending *nums[i]
+            v3 = maxending* nums[i]
+
+            maxending = max(v1,max(v2,v3))
+            minending = min(v1,min(v2,v3))
+
+            res = max(res,maxending)
+
+        return res
+
+        
+
+        
